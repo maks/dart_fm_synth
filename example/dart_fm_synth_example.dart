85a550b623476a8022d1718ceb65a302bc1da011
Maksim Lin <maks@manichord.com>
2021-07-25 17:10:40 +1000

    minimal FFI static call working

--- example/dart_fm_synth_example.dart
+++ example/dart_fm_synth_example.dart
@@ -1,5 +1,7 @@
 import 'package:dart_fm_synth/dart_fm_synth.dart';
 
 void main() {
-  DartFmSynth.init('../');
+  DartFmSynth.init('build/libfmsynth.so');
+  final synth = DartFmSynth(44100, 4);
+  print('libfmsynth version: ${DartFmSynth.version()}');
 }
--- lib/src/dart_fm_synth.dart
+++ lib/src/dart_fm_synth.dart
@@ -10,6 +10,8 @@ typedef _FMSynthFreeNative = Void Function(Pointer<_FMSynth> fm);
 typedef _FMSynthFree = void Function(Pointer<_FMSynth> _fm);
 typedef _FMSynthResetNative = Void Function(Pointer<_FMSynth> _fm);
 typedef _FMSynthReset = void Function(Pointer<_FMSynth> _fm);
+typedef _FMSynthVersionNative = Int8 Function();
+typedef _FMSynthVersion = int Function();
 
 class DartFmSynth {
   static late final DynamicLibrary _lib;
@@ -19,15 +21,21 @@ class DartFmSynth {
 
   late Pointer<_FMSynth> _nativeInstance;
 
+  static late _FMSynthVersion _version;
+
   /// Loads the libfmsynth library.
   static init(String path) {
-    DynamicLibrary.open(path);
+    _lib = DynamicLibrary.open(path);
     _new = _lib.lookupFunction<_FMSynthNewNative, _FMSynthNew>('fmsynth_new');
     _free =
         _lib.lookupFunction<_FMSynthFreeNative, _FMSynthFree>('fmsynth_free');
     _reset = _lib
         .lookupFunction<_FMSynthResetNative, _FMSynthReset>('fmsynth_reset');
+    _version = _lib.lookupFunction<_FMSynthVersionNative, _FMSynthVersion>(
+        'fmsynth_get_version');
   }
+
+  static int version() => _version();
 
   DartFmSynth(double sampleRate, int maxVoices) {
     _nativeInstance = _new(sampleRate, maxVoices);
