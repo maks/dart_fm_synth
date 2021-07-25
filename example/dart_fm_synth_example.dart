924b61689fc4b9698a1fc36eb33a532a245d91a0
Maksim Lin <maks@manichord.com>
2021-07-25 22:19:07 +1000

    wip on working example

--- example/dart_fm_synth_example.dart
+++ example/dart_fm_synth_example.dart
@@ -1,7 +1,36 @@
 import 'package:dart_fm_synth/dart_fm_synth.dart';
 
-void main() {
+void main(List<String> args) {
   DartFmSynth.init('build/libfmsynth.so');
-  final synth = DartFmSynth(44100, 4);
   print('libfmsynth version: ${DartFmSynth.version()}');
+  final fmsynth = DartFmSynth(44100, 4);
+
+  for (int i = 0; i < 8; i++)
+   {
+      fmsynth.setParameter(FMSYNTH_PARAM_MOD_TO_CARRIERS0 + i, (i + 1) & 7, 2.0f);
+      fmsynth.setParameter(FMSYNTH_PARAM_FREQ_MOD, i, i + 1.0f);
+      fmsynth.setParameter(FMSYNTH_PARAM_PAN, i, (i & 1) ? -0.5f : +0.5f);
+      fmsynth.setParameter(FMSYNTH_PARAM_CARRIERS, i, 1.0f);
+      fmsynth.setParameter(FMSYNTH_PARAM_LFO_FREQ_MOD_DEPTH, i, 0.15f);
+   }
+
+   for (int i = 0; i < 64; i++)
+   {
+      fmsynth.noteOn(i + 20, 127);
+   }
+
+   for (int i = 0; i < 10; i++)
+   {
+      final renderedSamples = fmsynth_render(2048);
+
+      if (file)
+      {
+         for (int x = 0; x < 2048; x++)
+         {
+            fprintf(file, "%12.6f %12.6f\n", left[x], right[x]);
+         }
+      }
+   }
+
+   fmsynth.close();
 }
--- lib/src/dart_fm_synth.dart
+++ lib/src/dart_fm_synth.dart
@@ -1,5 +1,7 @@
 import 'dart:ffi';
 
+import 'package:ffi/ffi.dart';
+
 class _FMSynth extends Opaque {}
 
 typedef _FMSynthNewNative = Pointer<_FMSynth> Function(
@@ -12,16 +14,30 @@ typedef _FMSynthResetNative = Void Function(Pointer<_FMSynth> _fm);
 typedef _FMSynthReset = void Function(Pointer<_FMSynth> _fm);
 typedef _FMSynthVersionNative = Int8 Function();
 typedef _FMSynthVersion = int Function();
+typedef _FMSynthSetParameterNative = Void Function(
+    Pointer<_FMSynth> _fm, Uint8 parameter, Uint8 operatorIndex, Float value);
+typedef _FMSynthSetParameter = void Function(
+    Pointer<_FMSynth> _fm, int parameter, int operatorIndex, double value);
+typedef _FMSynthRenderNative = Uint8 Function(Pointer<_FMSynth> _fm,
+    Pointer<Float> left, Pointer<Float> right, Uint8 frames);
+typedef _FMSynthRender = int Function(Pointer<_FMSynth> _fm,
+    Pointer<Float> left, Pointer<Float> right, int frames);
 
 class DartFmSynth {
   static late final DynamicLibrary _lib;
+
+  static late _FMSynthVersion _version;
+
   static late _FMSynthNew _new;
   static late _FMSynthFree _free;
   static late _FMSynthReset _reset;
+  static late _FMSynthSetParameter _setParameter;
+  static late _FMSynthRender _render;
 
   late Pointer<_FMSynth> _nativeInstance;
+  int _activeVoices = 0;
 
-  static late _FMSynthVersion _version;
+  int get activeVoices => _activeVoices;
 
   /// Loads the libfmsynth library.
   static init(String path) {
@@ -33,6 +49,11 @@ class DartFmSynth {
         .lookupFunction<_FMSynthResetNative, _FMSynthReset>('fmsynth_reset');
     _version = _lib.lookupFunction<_FMSynthVersionNative, _FMSynthVersion>(
         'fmsynth_get_version');
+    _setParameter =
+        _lib.lookupFunction<_FMSynthSetParameterNative, _FMSynthSetParameter>(
+            'fmsynth_set_parameter');
+    _render = _lib
+        .lookupFunction<_FMSynthRenderNative, _FMSynthRender>('fmsynth_render');
   }
 
   static int version() => _version();
@@ -44,4 +65,32 @@ class DartFmSynth {
   void close() => _free(_nativeInstance);
 
   void reset() => _reset(_nativeInstance);
+
+  void setParameter(int parameter, int operatorIndex, double value) =>
+      _setParameter(_nativeInstance, parameter, operatorIndex, value);
+
+  StereoBuffer render(int frames) {
+    final leftBuffer = calloc<Float>(frames);
+    final rightBuffer = calloc<Float>(frames);
+    _activeVoices = _render(
+      _nativeInstance,
+      leftBuffer,
+      rightBuffer,
+      frames,
+    );
+    final buffer = StereoBuffer(
+      List.from(leftBuffer.asTypedList(frames)),
+      List.from(rightBuffer.asTypedList(frames)),
+    );
+    calloc.free(leftBuffer);
+    calloc.free(rightBuffer);
+    return buffer;
+  }
+}
+
+class StereoBuffer {
+  final List<double> left;
+  final List<double> right;
+
+  StereoBuffer(this.left, this.right);
 }
